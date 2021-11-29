618fcf9fc8a6020e70a3979f5cffa054d027072f
ShubhamGupta15 <55907255+ShubhamGupta15@users.noreply.github.com>
2021-11-29 14:50:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,14 +53,14 @@ elif args.dataset=='cifar100':
                                                 transforms.ToTensor(),
                                                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                             ]))
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=3)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=5)
 
     testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True,
                                            transform=transforms.Compose([
                                                transforms.ToTensor(),
                                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                            ]))
-    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=3)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=5)
 
 if args.net_name=='dy_resnet18':
     model = dy_resnet18(num_classes=numclasses)
