eb3cb894ea215471a6b05ced1f45a5cd415f024b
ShubhamGupta15 <55907255+ShubhamGupta15@users.noreply.github.com>
2021-11-30 06:02:41 +0530

    Update dy_resnet.py

--- dynamic_models/dy_resnet.py
+++ dynamic_models/dy_resnet.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from dynamic_conv import Dynamic_conv2d
+from dynamic_conv import DyConv2d
 # from .utils import load_state_dict_from_url
 
 
@@ -15,14 +15,14 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
 #                      padding=dilation, groups=groups, bias=False, dilation=dilation)
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    return Dynamic_conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
+    return DyConv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
 
 # def conv1x1(in_planes, out_planes, stride=1):
 #     """1x1 convolution"""
 #     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 def conv1x1(in_planes, out_planes, stride=1):
-    return Dynamic_conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False,)
+    return DyConv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False,)
 
 
 
